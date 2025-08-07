923756225ad494f7dfcdf82a9aec40561a66633f
ziv8310 <elziv8310@gmail.com>
2025-08-07 21:23:49 +0300

    past test 26

--- malloc_1.cpp
+++ malloc_1.cpp
@@ -1,12 +1,13 @@
 #include <unistd.h>
+#include <cstring>
 
-void* smalloc(size_t size){
-     if(size==0 || size >10*10*10*10*10*10*10*10){
-         return nullptr;
-     }
-     void* pointer = std::sbrk(size);
-     if (pointer == (void*) -1) {
-         return nullptr;
-     }
-     return pointer;
- }
+void *smalloc(size_t size) {
+    if (size == 0 || size > 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10) {
+        return nullptr;
+    }
+    void *pointer = sbrk(size);
+    if (pointer == (void *) -1) {
+        return nullptr;
+    }
+    return pointer;
+}
--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,10 +1,11 @@
 #include <unistd.h>
+#include <cstring>
 
 struct MallocMetadata {
     size_t size;
     bool is_free;
-    MallocMetadata* next;
-    MallocMetadata* prev;
+    MallocMetadata *next;
+    MallocMetadata *prev;
 };
 
 size_t var_num_free_blocks = 0; //
@@ -13,18 +14,18 @@ size_t var_num_allocated_blocks = 0; //number of total blocks
 size_t var_num_allocated_bytes = 0;
 size_t var_num_meta_data_bytes = 0;
 
-size_t _size_meta_data(){
+size_t _size_meta_data() {
     return sizeof(MallocMetadata);
 }
 
-MallocMetadata* malloc_list = nullptr;
+MallocMetadata *malloc_list = nullptr;
 
-void* smalloc(size_t size) {
+void *smalloc(size_t size) {
     if (size == 0 || size > 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10) {
         return nullptr;
     }
     size_t new_full_size = size + _size_meta_data();
-    MallocMetadata* curr = malloc_list;
+    MallocMetadata *curr = malloc_list;
     void *pointer;
     MallocMetadata *new_meta;
 
@@ -32,21 +33,21 @@ void* smalloc(size_t size) {
     //TODO need to check for pointer fails, maybe make it a seperate function.
     // if list is empty
     if (curr == nullptr) {
-        pointer = std::sbrk(new_full_size); //TODO instead of sbrk, call wrapper function.
-        if (pointer == (void *)-1) {
+        pointer = sbrk(new_full_size); //TODO instead of sbrk, call wrapper function.
+        if (pointer == (void *) -1) {
             return nullptr;
         }
-        new_meta = (MallocMetadata *)pointer;
+        new_meta = (MallocMetadata *) pointer;
         new_meta->size = size;
         new_meta->is_free = false;
         new_meta->next = nullptr;
         new_meta->prev = nullptr;
         var_num_allocated_blocks++;
-        var_num_allocated_bytes += new_full_size;
+        var_num_allocated_bytes += size;
         var_num_meta_data_bytes += _size_meta_data();
-        // TODO return malloc aid func val
-    }
-    else {
+        new_meta++;
+        return new_meta;
+    } else {
         // if list isn't empty check if it has a free block of an appropriate size
         while (curr != nullptr) {
             if (curr->is_free && curr->size >= size) {
@@ -64,83 +65,85 @@ void* smalloc(size_t size) {
         // updates both new block and previous block
 
         // new block allocation
-        pointer = std::sbrk(new_full_size);
-        if (pointer == (void *)-1) {
+        pointer = sbrk(new_full_size);
+        if (pointer == (void *) -1) {
             return nullptr;
         }
 
-        new_meta = (MallocMetadata *)pointer;
+        new_meta = (MallocMetadata *) pointer;
         new_meta->size = size;
         new_meta->is_free = false;
         new_meta->next = nullptr;
         new_meta->prev = curr;
         var_num_allocated_blocks++;
-        var_num_allocated_bytes += new_full_size;
+        var_num_allocated_bytes += size;
         var_num_meta_data_bytes += _size_meta_data();
 
         // old block fix
         curr->next = new_meta;
-        return new_meta+1;
+        new_meta++;
+        return new_meta;
     }
+    return nullptr;
 }
 
-void* scalloc(size_t num, size_t size){
-    void* p = smalloc(size*num);
+void *scalloc(size_t num, size_t size) {
+    void *p = smalloc(size * num);
     if (p == nullptr) {
         return nullptr;
     }
-    std::memset(p, 0, size*num);
+    memset(p, 0, size * num);
     return p;
 }
 
-void sfree(void* p){
-    if (p == nullptr){
+void sfree(void *p) {
+    if (p == nullptr) {
         return;
     }
-    MallocMetadata* meta = static_cast<MallocMetadata*>(p) - 1;
-    meta->is_free=true;
+    MallocMetadata *meta = static_cast<MallocMetadata *>(p) - 1;
+    meta->is_free = true;
     var_num_free_blocks++;
     var_num_free_bytes += meta->size;
 }
 
 
-void* srealloc(void* oldp, size_t size){
+void *srealloc(void *oldp, size_t size) {
     if (size == 0 || size > 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10) {
         return nullptr;
     }
-    if (oldp == nullptr){
+    if (oldp == nullptr) {
         return smalloc(size);
     }
-    MallocMetadata* old_meta = static_cast<MallocMetadata*>(oldp) - 1;
-    if(old_meta->size >=size){
+    MallocMetadata *old_meta = static_cast<MallocMetadata *>(oldp) - 1;
+    if (old_meta->size >= size) {
         return oldp;
     }
 
-    void* new_mem = smalloc(size);
+    void *new_mem = smalloc(size);
     if (new_mem != nullptr) {
-        std::memmove(new_mem, oldp, size);
+        memmove(new_mem, oldp, size);
         return new_mem;
     } else {
         return nullptr;
     }
 }
 
-size_t _num_free_blocks(){
+size_t _num_free_blocks() {
     return var_num_free_blocks;
 }
 
-size_t _num_free_bytes(){
+size_t _num_free_bytes() {
     return var_num_free_bytes;
 }
 
-size_t _num_allocated_blocks(){
+size_t _num_allocated_blocks() {
     return var_num_allocated_blocks;
 }
 
-size_t _num_allocated_bytes(){
+size_t _num_allocated_bytes() {
     return var_num_allocated_bytes;
 }
 
-size_t _num_meta_data_bytes(){
+size_t _num_meta_data_bytes() {
     return var_num_meta_data_bytes;
 }
